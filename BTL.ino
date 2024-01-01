b25ea3afd03b0c8521f0a45831d9fd6ceed893b8
DESKTOP-R201FAL\Thinkpad <trinhanhai1710@gmail.com>
2024-01-01 16:50:25 +0700

    Add assets and README.md

--- BTL.ino
+++ BTL.ino
@@ -17,11 +17,20 @@ float updateRate;
 int highScore = 0;
 const int buttonPins[] = {2, 3, 4, 5};
 bool gameOver;
-bool appleDisplay = true;
+bool isAppleDisplayed;
+bool isDisplayChanged;
 byte gameMap[MAP_SIZE]; // The 8 rows of the LED Matrix
 
 #define OLED_ADDR   0x3C
 Adafruit_SSD1306 display(128, 64, &Wire, -1);
+
+const int DIN = 11;
+const int CS = 10;
+const int CLK = 13;
+LedControl lc = LedControl(DIN, CLK, CS, 1);
+
+// Variables To Handle The Game Time
+float oldTime, timer;
 
 struct Point {
   int rPos; // The row of the point
@@ -58,20 +67,23 @@ void emitSound(int condition) {
 }
 
 struct Snake {
-  Point head;     // The head of the snake's head
   Point body[MAX_SCORE]; // An array that contains the coordinates of the the snake's body
   int len;         // The length of the snake 
   int dir[2];      // A direction to move the snake along
 
+  Point getHead() {
+    return body[len - 1];
+  }
+
   void move() {
-    Point newHead = head.add(dir[0], dir[1]);
+    Point newHead = getHead().add(dir[0], dir[1]);
   
     // Check if the Snake hits itself
     for(int i = 0; i < len; i++) {
       if(body[i].equals(newHead)) {
         emitSound(DIE);
         gameOver = true;
-        display.clearDisplay();
+        isDisplayChanged = true;
         return;
       }
     }
@@ -85,7 +97,7 @@ struct Snake {
       }
       apple = genApple();
       updateRate += 0.1;
-      display.clearDisplay();
+      isDisplayChanged = true;
     } else {
       // Shift the body forward
       for(int i = 1; i < len; i++)
@@ -94,27 +106,19 @@ struct Snake {
 
     // Append the new head to the body
     body[len - 1] = newHead;
-    head = newHead;
   }
 };
 Snake snake;
 
-//MAX72XX led Matrix
-const int DIN = 11;
-const int CS = 10;
-const int CLK = 13;
-LedControl lc = LedControl(DIN, CLK, CS, 1);
-
-// Variables To Handle The Game Time
-float oldTime, timer;
-
 void initGame() {
   display.clearDisplay();
-  snake = {{1, 5}, {{0, 5}, {1, 5}}, 2, {1, 0}}; // Initialize a snake object
+  snake = {{{0, 5}, {1, 5}}, 2, {1, 0}}; // Initialize a snake object
   apple = genApple(); // Initialize an apple object
   oldTime = millis();
   timer = 0;
   updateRate = INIT_UPDATE_RATE;
+  isAppleDisplayed = true;
+  isDisplayChanged = true;
   gameOver = false;
 }
 
@@ -122,11 +126,8 @@ void setup() {
   display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR);
   display.setTextColor(WHITE);
   display.setTextSize(1);
+
   pinMode(SPEAKER_PIN, OUTPUT);
-  /*
-  The MAX72XX is in power-saving mode on startup,
-  we have to do a wakeup call
-  */
   lc.shutdown(0, false);
   /* Set the brightness to a medium values */
   lc.setIntensity(0, 8);
@@ -161,14 +162,18 @@ Point genApple() {
   return newPoint;
 }
 
-void checkGameState() {
-  int score = snake.len;
-  if (score < MAX_SCORE) {
-    printDisplay(1, ("Your score: " + String(score)).c_str());
-    printDisplay(2, ("High score: " + String(highScore)).c_str());
-  } else {
-    emitSound(WIN);
-    printDisplay(1, "You win!");
+void displayGameState() {
+  if (isDisplayChanged) {
+    display.clearDisplay();
+    int score = snake.len;
+    if (score < MAX_SCORE) {
+      printDisplay(1, ("Your score: " + String(score)).c_str());
+      printDisplay(2, ("High score: " + String(highScore)).c_str());
+    } else {
+      emitSound(WIN);
+      printDisplay(1, "You win!");
+    }
+    isDisplayChanged = false;
   }
 }
 
@@ -200,11 +205,11 @@ void Update() {
   float dt = currentTime - oldTime;
   oldTime = currentTime;
   timer += dt;
-  if(timer < DELAY / (2 * updateRate) && !appleDisplay) {
-    appleDisplay = true;
+  if(timer < DELAY / (2 * updateRate) && !isAppleDisplayed) {
+    isAppleDisplayed = true;
   }
-  if(timer > DELAY / (2 * updateRate) && appleDisplay) {
-    appleDisplay = false;
+  if(timer > DELAY / (2 * updateRate) && isAppleDisplayed) {
+    isAppleDisplayed = false;
   }
   if(timer > DELAY / updateRate){
     timer = 0;
@@ -219,7 +224,7 @@ void Render() {
   for(int i = 0; i < snake.len; i++)
     gameMap[snake.body[i].rPos] |= 128 >> snake.body[i].cPos;
   
-  if(appleDisplay)
+  if(isAppleDisplayed)
     gameMap[apple.rPos] |= 128 >> apple.cPos;
 
   for(int i = 0; i < MAP_SIZE; i++)
@@ -234,9 +239,9 @@ void loop() {
     if(isClicked())
       initGame();
   } else {
-    checkGameState();
     readInstruction();
     Update();
     Render();
+    displayGameState();
   }
 }
---
+++ README.md
@@ -0,0 +1,70 @@
+# Trò chơi rắn săn mồi
+[Project trên Wokwi](https://wokwi.com/projects/383641267625734145)
+
+## Video demo:
+![Video demo](assets/demo.gif)
+## Phần cứng
+- 1 Arduino Uno
+- 1 ma trận LED 8x8
+- 4 nút điều khiển (Lên, Xuống, Trái, Phải)
+- 1 loa 
+
+Sơ đồ ghép nối các thiết bị:
+![Sơ đồ ghép nối các thiết bị](assets/diagram.png)
+
+## Thư viện
+- LedControl: Hiển thị giao diện trò chơi trên ma trận Led 8x8
+- Adafruit SSD1306: Hiển thị điểm trên màn hình LCD
+
+## Code
+### Mô hình táo và rắn
+Màn hình trò chơi được biểu diễn bởi mảng 2 chiều 8x8 có hệ tọa độ như trong hình dưới đây:
+![Hệ tọa độ màn hình LED](assets/led_matrix_schematic.png)
+Mỗi LED được biểu diễn bởi 1 struct *Point* có 2 trường thuộc tính *rPos* và *cPos* thể hiển chỉ số hàng và chỉ số cột của LED đó:
+```
+struct Point {
+    int rPos; // The row of the point
+    int cPos; // The column of the point
+    ...
+}
+```
+Táo được biểu diễn dưới dạng 1 *Point*.
+
+Rắn được biểu diễn bởi một mảng *Point* theo thứ tự từ đuôi đến đầu. Ngoài ra, rắn cũng cần một mảng 2 chiều thể hiện hướng di chuyển của mình theo hàng và theo cột và một biến thể hiện chiều dài của rắn. Tất cả thông tin này được lưu vào trong struct *Snake* như sau:
+```
+struct Snake {
+    Point body[MAX_SCORE]; // An array that contains the coordinates of the the snake's body
+    int len;         // The length of the snake 
+    int dir[2];      // A direction to move the snake along
+    ...
+}
+```
+### Vòng lặp game
+Vòng lặp chính của game gồm 4 bước:
+- Đọc đầu vào từ các nút bấm, nếu có (*readInstruction*)
+- Cập nhật các thuộc tính của táo và rắn (*Update*)
+- Hiển thị màn hình game trên ma trận LED (*Render*)
+- Hiển thị điểm số của game trên màn hình LCD (*displayGameState*)
+
+Khi trò chơi kết thúc, người dùng có thể bấm 1 nút bất kỳ để bắt đầu ván chơi mới
+
+### Di chuyển của rắn
+Dựa vào hướng di chuyển và vị trí đầu cũ của rắn có thể tính được vị trí đầu mới của rắn.
+
+Như đã nói ở phần trên, mảng **body** được lưu theo thứ tự từ đuôi tới đầu. Điều này thuận lợi cho việc cập nhật khi rắn di chuyển bởi ta chỉ cần dịch trái mảng này và thêm đầu mới vào cuối mảng này (thêm vào cuối 1 mảng dễ hơn so với thêm vào đầu). 
+
+Ngoài ra, rắn phải kiểm tra hai trường hợp có thể xảy ra khi di chuyển:
+- Rắn tự đâm vào bản thân: Lặp qua mảng body, kiểm tra xem xem có vị trí nào trùng với đầu mới không 
+- Rắn ăn táo: Kiểm tra vị trí của táo có trùng với vị trí của đầu rắn không. Trong trường hợp rắn ăn được táo thì chỉ cần thêm vị trí mới này vào cuối mảng **body**
+
+### Hiển thị trên ma trận LED 8x8
+Đầu tiên, màn hình LED sẽ được xóa trắng. Sau đó, tọa độ của các LED hiển thị mới được tính thông qua táo và rắn. Cuối cùng, sử dụng thư viện *LedControl* để cập nhật các thay đổi này
+
+Táo có tốc độ hiển thị (*updateRate*) nhanh gấp 2 so với rắn để tạo hiệu ứng nhấp nháy.
+
+
+### Loa
+Loa sẽ phát ra 3 tiếng kêu khác nhau tương ứng với 3 sự kiện:
+- Rắn ăn được táo (**EAT**)
+- Rắn chết (**DIE**)
+- Rắn thắng (**WIN**)
